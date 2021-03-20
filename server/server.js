62b5656643db2174a060cfa6e41ec40f6083f7a5
zachlloyd01 <zachlloyd01@gmail.com>
2021-03-20 16:36:44 -0700

    minor changes

--- server/server.js
+++ server/server.js
@@ -8,8 +8,10 @@ app.use(express.json());
 
 app.use(cors());
 
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/api', require('./apiRoutes')); // Pass api requests to the API routes
 
 const port = process.env.PORT || 8080;
 
-app.listen(port, () => console.log(`server is running on port ${port}`))
+app.listen(port, () => console.log(`Server is running on port ${port}`))
