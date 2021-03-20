18ebaab6af2e1955a193624082ea89c015144372
zachlloyd01 <zachlloyd01@gmail.com>
2021-03-20 16:03:33 -0700

    sessions and tutors REST routes

--- server/apiRoutes/sessions.js
+++ server/apiRoutes/sessions.js
@@ -6,20 +6,21 @@ const { Sessions } = require('../models');
 
 router.post('/', async function(req, res) { // Create new session
     try { // Error handling
-        
-        
+        const newSession = await Sessions.create(req.body); // Create a new session with the data in the request
+        res.status(200).json(newSession); 
     }
     catch(err) { // If it errors, respond with an error
-        
+        res.status(500).json(err);
     } 
 });
 
 router.get('/', async function(req, res) { // Retrieve all sessions
     try {
-        
+        const sessions = await Sessions.findAll();
+        res.status(200).json(sessions);
     }
     catch(err) {
-        
+        res.status(500).json(err);
     }
     
 });
@@ -27,20 +28,25 @@ router.get('/', async function(req, res) { // Retrieve all sessions
 router.get('/:id', async function(req, res) { // Retrieve a specific session
     try {
         const { id } = req.params;
-        
+        const foundSession = await Sessions.findOne({ where: { id } });
+        res.status(200).json(foundSession);
     } 
     catch (error) {
-        
+        res.status(500).json(error);
     }
     
 });
 
 router.put('/:id', async function(req, res) { // Update a session
     try {
-        
+        const updatedSession = await Sessions.update(
+            req.body,
+            { where: { id } },
+        );
+        res.status(200).json(updatedSession);
     } 
     catch (error) {
-        
+        res.status(500).json(error);
     }
     
 });
@@ -48,9 +54,15 @@ router.put('/:id', async function(req, res) { // Update a session
 router.delete('/:id', async function(req, res) { // Delete a session
     try {
         const { id } = req.params;
+        await Sessions.destroy({
+            where: {
+                id
+            }
+        });
+        res.status(202).json('')
     }
     catch(err) {
-        
+        res.status(500).json(err);
     }
 });
 
--- server/apiRoutes/tutors.js
+++ server/apiRoutes/tutors.js
@@ -3,22 +3,24 @@
 const router = require('express').Router();
 const { Tutors } = require('../models');
 
+
 router.post('/', async function(req, res) { // Create new tutor
     try { // Error handling
-        
-        
+        const newTutor = await Tutors.create(req.body); // Create a new tutor with the data in the request
+        res.status(200).json(newTutor); 
     }
     catch(err) { // If it errors, respond with an error
-        
+        res.status(500).json(err);
     } 
 });
 
 router.get('/', async function(req, res) { // Retrieve all tutors
     try {
-        
+        const tutors = await Tutors.findAll();
+        res.status(200).json(tutors);
     }
     catch(err) {
-        
+        res.status(500).json(err);
     }
     
 });
@@ -26,20 +28,25 @@ router.get('/', async function(req, res) { // Retrieve all tutors
 router.get('/:id', async function(req, res) { // Retrieve a specific tutor
     try {
         const { id } = req.params;
-        
+        const foundTutor = await Tutors.findOne({ where: { id } });
+        res.status(200).json(foundTutor);
     } 
     catch (error) {
-        
+        res.status(500).json(error);
     }
     
 });
 
 router.put('/:id', async function(req, res) { // Update a tutor
     try {
-        
+        const updatedTutor = await Tutors.update(
+            req.body,
+            { where: { id } },
+        );
+        res.status(200).json(updatedTutor);
     } 
     catch (error) {
-        
+        res.status(500).json(error);
     }
     
 });
@@ -47,9 +54,15 @@ router.put('/:id', async function(req, res) { // Update a tutor
 router.delete('/:id', async function(req, res) { // Delete a tutor
     try {
         const { id } = req.params;
+        await Tutors.destroy({
+            where: {
+                id
+            }
+        });
+        res.status(202).json('')
     }
     catch(err) {
-        
+        res.status(500).json(err);
     }
 });
 
