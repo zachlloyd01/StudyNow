3a20ac96008fc0b0fdb76d4d0c43bb07eb6d52ac
zachlloyd01 <zachlloyd01@gmail.com>
2021-04-02 22:22:37 -0700

    session management middleware

--- server/apiRoutes/index.js
+++ server/apiRoutes/index.js
@@ -1,4 +1,24 @@
 const router = require('express').Router();
+const jwt = require('jsonwebtoken');
+
+/* AUTH CHECK MIDDLEWARE */
+router.use(async function(req, res, next) {
+    newRefreshToken = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+    if(req.originalUrl === '/api/profiles' && req.method === 'POST') { // Signing In / Signing Up
+        next();
+    }
+    else { // Not signing in/up, need to check auth
+        try {
+            const decoded = await jwt.verify(req.headers.sessionToken, process.env.JWT_SECRET);
+            
+        }
+        catch(error) {
+            console.error(error);
+        }
+        
+    }
+    
+});
 
 router.use('/profiles', require('./profiles'));
 router.use('/sessions', require('./sessions'));
